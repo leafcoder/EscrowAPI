c6fdeed6ec8f7314db00e78090b6b4e2a822b930
leafcoder <leafcoder@gmail.com>
2018-01-17 00:05:08 +0800

    日常更新

--- escrow.py
+++ escrow.py
@@ -11,6 +11,8 @@ import sys
 sys.dont_write_bytecode = True
 
 import requests
+from posixpath import join as posix_join
+from weakref import proxy as weakref_proxy
 from api_config import ESCROW_SECRET, ESCROW_API_KEY
 
 # See below for a list of HTTP codes we return.
@@ -27,23 +29,123 @@ error_codes = {
 
 class Authorization(object):
 
-    def __init__(self, api_secret, api_key, account_email, password=None):
-        self.api_secret = api_secret
-        self.api_key = api_key
+    def __init__(self, api_secret, api_key, account_email, password):
         self.account_email = account_email
+        self.api_secret = api_secret
         self.password = password
+        self.api_key = api_key
+
+def parse_object(data):
+    if isinstance(data, list):
+        tmp = []
+        for value in data:
+            if isinstance(value, (list, tuple, dict)):
+                tmp.append(parse_object(value))
+            elif isinstance(value, Base):
+                tmp.append(value.to_dict())
+            else:
+                tmp.append(value)
+        return tmp
+    if isinstance(data, dict):
+        tmp = {}
+        for key, value in data.items():
+            if isinstance(value, (list, tuple, dict)):
+                tmp[key] = parse_object(value)
+            elif isinstance(value, Base):
+                tmp[key] = value.to_dict()
+            else:
+                tmp[key] = value
+        return tmp
+    if isinstance(data, Base):
+        return data.to_dict()
+    return data
+
+class Base(object):
+
+    def to_dict(self):
+        return parse_object(self.__dict__)
+
+class Transaction(Base):
+
+    def __init__(self, escrowapi, buyer_email=None, seller_email=None,
+                       currency='usd', description=None):
+        self.escrowapi = weakref_proxy(escrowapi)
+        self.buyer_email = buyer_email
+        self.seller_email = seller_email
+        self.currency = currency
+        self.description = description
+        self.items = []
+
+    def add_item(self, transaction_item):
+        self.items.append(transaction_item)
+
+    def finish(self):
+        api_url = self.escrowapi.create_api_url('transaction')
+        response = requests.post(
+            api_url, auth=self.auth, json=self.to_dict()
+        )
+        if 200 != status_code:
+            text = response.text or error_codes[status_code]
+            raise EscrowAPIError(text)
+        return response.json()
+
+    def to_dict(self):
+        return {
+            'parties': [
+                {
+                    'role': 'buyer',
+                    'customer': self.buyer_email
+                },
+                {
+                    'role': 'seller',
+                    'customer': self.seller_email
+                }
+            ],
+            'currency': self.currency,
+            'description': self.description,
+            'items': parse_object(self.items)
+        }
+        return parse_object(self.__dict__)
+
+class TransactionItem(Base):
+
+    def __init__(self, title=None, description=None, type=None,
+                       inspection_period=None, quantity=None):
+        self.title = title
+        self.description = description
+        self.type = type
+        self.inspection_period = inspection_period
+        self.quantity = quantity
+        self.schedule = []
+
+    def add_schedule(self, amount=None, payer_customer=None,
+                           beneficiary_customer=None):
+        item_schedule = TransactionItemSchedule(
+            amount=amount, payer_customer=payer_customer,
+            beneficiary_customer=beneficiary_customer
+        )
+        self.schedule.append(item_schedule)
+
+class TransactionItemSchedule(Base):
+
+    def __init__(self, amount=None, payer_customer=None,
+                       beneficiary_customer=None):
+        self.amount = amount
+        self.payer_customer = payer_customer
+        self.beneficiary_customer = beneficiary_customer
 
 class EscrowAPI(object):
 
-    def __init__(self, api_baseurl,
-                       api_secret,
-                       api_key,
-                       account_email,
+    def __init__(self, api_baseurl, api_secret, api_key, account_email,
                        password=None):
         self.authorization = Authorization(
-            api_secret, api_key, account_email, password=password
-        )
+            api_secret, api_key, account_email, password)
         self.api_baseurl = api_baseurl
+
+    @property
+    def auth(self):
+        authorization = self.authentication
+        return authorization.account_email, authorization.api_key
 
     @property
     def api_secret(self):
@@ -61,69 +163,16 @@ class EscrowAPI(object):
     def password(self):
         return self.authorization.password
 
-    def customer(self, email, first_name=None,
-                              middle_name=None,
-                              last_name=None,
-                              line1=None,
-                              line2=None,
-                              city=None,
-                              state=None,
-                              country=None,
-                              post_code=None,
-                              post_number=None):
-        """Creating a new customer on Escrow.com
-        
-        You are only able to create customers where their email address is 
-        not already in use. Creating an account for a user that already 
-        exists will result in an error, and an HTTP 403 will be returned.
-        Once a customer has been created, either through the API or if 
-        they've signed up themselves, their information can no longer be 
-        provided by API integrations.
+    def create_api_url(self, *args):
+        args = filter(None, map(lambda s: str(s).strip('/'), args))
+        return posix_join(self.api_baseurl, *args)
 
-        Once a customer has been created, they will then receive an email 
-        introducing them to Escrow.com and to log in and set their password.
-
-        Demo:
-            import requests
-            requests.post(
-                'https://api.escrow.com/2017-09-01/customer',
-                auth=('account-email', 'api-key')
-                json={
-                    "email": "john@escrow.com",
-                    "first_name": "John",
-                    "middle_name": "Kane",
-                    "last_name": "Smith",
-                    "address": {
-                        "line1": "1829 West Lane",
-                        "line2": "Apartment 301020",
-                        "city": "San Francisco",
-                        "state": "CA",
-                        "country": "US",
-                        "post_code": "10203"
-                    },
-                    "phone_number": "8885118600"
-                }
-            )
-
-        Arguments:
-            email {[type]} -- [description]
-        
-        Keyword Arguments:
-            first_name {[type]} -- [description] (default: {None})
-            middle_name {[type]} -- [description] (default: {None})
-            last_name {[type]} -- [description] (default: {None})
-            line1 {[type]} -- [description] (default: {None})
-            line2 {[type]} -- [description] (default: {None})
-            city {[type]} -- [description] (default: {None})
-            state {[type]} -- [description] (default: {None})
-            country {[type]} -- [description] (default: {None})
-            post_code {[type]} -- [description] (default: {None})
-            post_number {[type]} -- [description] (default: {None})
-        
-        Returns:
-            [type] -- [description]
-        """
-        json = {
+    def customer(self, email, first_name=None, middle_name=None,
+                       last_name=None, line1=None, line2=None, city=None,
+                       state=None, country=None, post_code=None,
+                       post_number=None):
+        api_url = self.create_api_url('/customer')
+        response = requests.post(api_url, auth=self.auth, json={
             'email': email,
             'first_name': first_name,
             'middle_name': middle_name,
@@ -137,147 +186,48 @@ class EscrowAPI(object):
                 'post_code': post_code
             },
             'phone_number': phone_number
-        }
-        account_email = self.account_email
-        api_key = self.api_key
-        api_baseurl = self.api_baseurl
-        response = requests.post(
-            '%(api_baseurl)scustomer' % { 'api_baseurl': api_baseurl },
-            auth=(account_email, api_key),
-            json=json
-        )
+        })
         status_code = response.status_code
-        print response.headers
         if 200 != status_code:
             text = response.text or error_codes[status_code]
             raise EscrowAPIError(text)
         return response.json()
 
     def me(self):
-        """Authentication with API keys
-
-        From within the account settings page, you can generate API keys 
-        associated with your account. This is preferable to using your user 
-        name and password, as it allows you to recycle your credentials 
-        without changing the password on your account. Using an API key 
-        is almost identical to using a username and password for 
-        authentication. Simply provide your API key as your password and 
-        we will handle the rest.
-
-        Demo:
-            import requests
-            requests.get(
-                'https://api.escrow.com/2017-09-01/customer/me',
-                auth=('account-email', 'api-key')
-            )
-        
-        Returns:
-            [type] -- [description]
-        
-        Raises:
-            EscrowAPIError -- [description]
-        """
-        account_email = self.account_email
-        api_key       = self.api_key
-        api_baseurl   = self.api_baseurl
-        response = requests.get(
-            '%(api_baseurl)scustomer/me' % {
-                'api_baseurl': api_baseurl
-            },
-            auth=(account_email, api_key),
-        )
+        api_url  = self.create_api_url('/customer/me')
+        response = requests.get(api_url, auth=self.auth)
         status_code = response.status_code
-        print response.headers
         if 200 != status_code:
             text = response.text or error_codes[status_code]
             raise EscrowAPIError(text)
         return response.json()
 
     def transaction_as_customer(self, transaction_id, as_customer, action):
-        """Performing actions on behalf of customers in a transaction
-        
-        The way that this is accomplished is by setting the As-Customer 
-        header to the email address of the party you want to perform 
-        the action on behalf of.
-
-        Demo:
-            import requests
-            requests.post(
-                'https://api.escrow.com/2017-09-01/customer',
-                auth=('email-address', 'api-key'),
-                json={
-                    "email": "john@escrow.com",
-                    "first_name": "John",
-                    "middle_name": "Kane",
-                    "last_name": "Smith",
-                    "address": {
-                        "line1": "1829 West Lane",
-                        "line2": "Apartment 301020",
-                        "city": "San Francisco",
-                        "state": "CA",
-                        "country": "US",
-                        "post_code": "10203"
-                    },
-                    "phone_number": "8885118600"
-                }
-            )
-        
-        Arguments:
-            transaction_id {[type]} -- [description]
-            as_customer {[type]} -- [description]
-            action {[type]} -- [description]
-        
-        Returns:
-            [type] -- [description]
-        """
-        account_email = self.account_email
-        api_key       = self.api_key
-        api_baseurl   = self.api_baseurl
+        api_url = self.create_api_url('transaction', transaction_id)
         headers = { 'As-Customer': as_customer }
-        json = { 'action': action }
+        json    = { 'action': action }
         response = requests.patch(
-            '%(api_baseurl)stransaction/%(transaction_id)s' % {
-                'api_baseurl'   : api_baseurl,
-                'transaction_id': transaction_id
-            },
-            auth=(account_email, api_key),
-            headers=headers,
-            json=json
-        )
+            api_url, auth=self.auth, headers=headers, json=json)
         status_code = response.status_code
-        print response.headers
         if 200 != status_code:
             text = response.text or error_codes[status_code]
             raise EscrowAPIError(text)
         return response.json()
 
-    def transaction(self, id, as_customer=None, action=None):
-        """[summary]
-        
-        [description]
-        
-        Demo:
-            import requests
+    def get_transaction(self, transaction_id):
+        api_url = self.create_api_url('transaction', transaction_id)
+        response = requests.get(api_url, auth=self.auth)
+        if 200 != status_code:
+            text = response.text or error_codes[status_code]
+            raise EscrowAPIError(text)
+        return response.json()
 
-            requests.get(
-                'https://api.escrow.com/2017-09-01/transaction/100',
-                auth=('john.wick@escrow.com', 'Escrow1234'),
-            )
-
-        Arguments:
-            id {[type]} -- [description]
-        
-        Keyword Arguments:
-            as_customer {[type]} -- [description] (default: {None})
-            action {[type]} -- [description] (default: {None})
-        """
-        response = requests.get(
-            '%(api)stransaction/100' % { 'api': api },
-            auth=(account, password)
+    def create_transaction(self, seller_email, currency='usd',
+                                 description=None):
+        return Transaction(
+            self, buyer_email='me', seller_email=seller_email,
+            currency=currency, description=description
         )
-        status_code = response.status_code
-        text = response.text
-        print status_code, text
 
 def main():
     if len(sys.argv) < 2 or 'sandbox' == sys.argv[1].lower():
@@ -295,7 +245,26 @@ def main():
     )
 
     # escrowapi.transaction()
-    print escrowapi.me()
+    # print escrowapi.me()
+    # 
+    transaction = Transaction(
+        buyer_email='me', seller_email='keanu.reaves@escrow.com',
+        currency='usd', description='johnwick.com'
+    )
+    transaction_item = TransactionItem(
+        title='johnwick.com', description='johnwick.com',
+        type='domain_name', inspection_period=259200, quantity=1
+    )
+    transaction_item.add_schedule(
+        amount=1000, payer_customer='me',
+        beneficiary_customer='keanu.reaves@escrow.com'
+    )
+    transaction.add_item(transaction_item)
+
+    ret = transaction_item.to_dict()
+    ret = transaction.to_dict()
+    import json
+    print json.dumps(ret, indent=4)
 
 if '__main__' == __name__:
     main()
